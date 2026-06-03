83773872646d359313576428a05370214d6380a5
Rolando Quiros <rjquiros21@gmail.com>
2026-06-02 20:17:28 -0600

    Paso 2 realizado

--- shared/src/commonMain/kotlin/io/github/kevinah95/myapplication/screens/ContadorScreen.kt
+++ shared/src/commonMain/kotlin/io/github/kevinah95/myapplication/screens/ContadorScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -18,7 +22,7 @@ import androidx.compose.ui.unit.dp
 //             ¿Por qué ocurre esto? Agrega un comentario explicando el problema.
 @Composable
 fun ContadorScreen() {
-    var count = 0 // No funciona: porque las funciones @Composable se vuelven a ejecutar cuando las UI necesita actualizarse.
+    var count by remember{mutableStateOf(0)} // No funciona: porque las funciones @Composable se vuelven a ejecutar cuando las UI necesita actualizarse.
 
     Column(
         modifier = Modifier
